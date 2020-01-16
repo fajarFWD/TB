c5e78b436d8bc8da98cbcc0f2fae2eb23c9a8f77
Elias Benbourenane <54410649+eliasbenb@users.noreply.github.com>
2020-01-16 20:32:36 +0400

    Updated trackers

--- aria.sh
+++ aria.sh
@@ -1,3 +1,4 @@
+export trackers="[udp://tracker.coppersurfer.tk:6969/announce,udp://tracker.leechers-paradise.org:6969/announce,udp://tracker.opentrackr.org:1337/announce,udp://p4p.arenabg.com:1337/announce,udp://9.rarbg.to:2710/announce,udp://9.rarbg.me:2710/announce,udp://tracker.pomf.se:80/announce,udp://tracker.openbittorrent.com:80/announce,udp://exodus.desync.com:6969/announce,udp://tracker.tiny-vps.com:6969/announce,udp://tracker.moeking.me:6969/announce,udp://retracker.lanta-net.ru:2710/announce,udp://open.stealth.si:80/announce,udp://denis.stalker.upeer.me:6969/announce,udp://tracker.torrent.eu.org:451/announce,udp://tracker.cyberia.is:6969/announce,udp://open.demonii.si:1337/announce,udp://ipv4.tracker.harry.lu:80/announce,udp://tracker3.itzmx.com:6961/announce,udp://zephir.monocul.us:6969/announce]"
 export MAX_DOWNLOAD_SPEED=0
 export MAX_CONCURRENT_DOWNLOADS=3
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
@@ -5,3 +6,4 @@ aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+   --bt-tracker=$trackers
